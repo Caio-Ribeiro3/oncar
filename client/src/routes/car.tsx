848ea8a7aece66792e75171461a6056913158a69
Caio Ribeiro <kaio03693690@gmail.com>
2024-02-24 19:32:00 -0300

    remove variables not used

--- client/src/routes/car.tsx
+++ client/src/routes/car.tsx
@@ -32,41 +32,41 @@ import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded';
 
 import { fetcher } from '../facade/fetcher';
 
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-    if (b[orderBy] < a[orderBy]) {
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-        return 1;
-    }
-    return 0;
-}
+// function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+//     if (b[orderBy] < a[orderBy]) {
+//         return -1;
+//     }
+//     if (b[orderBy] > a[orderBy]) {
+//         return 1;
+//     }
+//     return 0;
+// }
 
 type Order = 'asc' | 'desc';
 
-function getComparator<Key extends keyof any>(
-    order: Order,
-    orderBy: Key,
-): (
-    a: { [key in Key]: number | string },
-    b: { [key in Key]: number | string },
-) => number {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
+// function getComparator<Key extends keyof any>(
+//     order: Order,
+//     orderBy: Key,
+// ): (
+//     a: { [key in Key]: number | string },
+//     b: { [key in Key]: number | string },
+// ) => number {
+//     return order === 'desc'
+//         ? (a, b) => descendingComparator(a, b, orderBy)
+//         : (a, b) => -descendingComparator(a, b, orderBy);
+// }
 
-function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
-    const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) {
-            return order;
-        }
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
-}
+// function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
+//     const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+//     stabilizedThis.sort((a, b) => {
+//         const order = comparator(a[0], b[0]);
+//         if (order !== 0) {
+//             return order;
+//         }
+//         return a[1] - b[1];
+//     });
+//     return stabilizedThis.map((el) => el[0]);
+// }
 
 function RowMenu() {
     return (
@@ -112,8 +112,8 @@ export function loader() {
 
 export default function CarRoute() {
     const cars = useLoaderData() as Awaited<ReturnType<typeof loader>>
-    const [order, setOrder] = React.useState<Order>('desc');
-    const [selected, setSelected] = React.useState<readonly string[]>([]);
+    // const [order, setOrder] = React.useState<Order>('desc');
+    // const [selected, setSelected] = React.useState<readonly string[]>([]);
     const [open, setOpen] = React.useState(false);
 
     const renderFilters = () => (
